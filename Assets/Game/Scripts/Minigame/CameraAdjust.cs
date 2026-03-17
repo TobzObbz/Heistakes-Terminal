2b19e43f9b3f0f48ed6d6a050ef1489b23d4c4f3
Toby Yaxley <tobyyaxley@virginmedia.com>
2026-03-17 21:17:42 +0000

    Adjusted levels and fixed aspect ratio issues

--- Assets/Game/Scripts/Minigame/CameraAdjust.cs
+++ Assets/Game/Scripts/Minigame/CameraAdjust.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraAdjust : MonoBehaviour
 {
-    private float targetAspect = 16f / 9f;
+    private float targetWidth = 1920f;
+    private float targetHeight = 1080f;
+
     private Camera cam;
 
     private void Start()
     {
         cam = GetComponent<Camera>();
 
+        float targetAspect = targetWidth / targetHeight;
         float windowAspect = (float)Screen.width / Screen.height;
-        float scaleHeight = windowAspect / targetAspect;
 
-        if (scaleHeight < 1.0f)
+        if (windowAspect >= targetAspect)
         {
-            cam.orthographicSize = cam.orthographicSize / scaleHeight;
+            cam.orthographicSize = targetHeight / 200f;
+        }
+        else
+        {
+            float scaleHeight = targetAspect / windowAspect;
+            cam.orthographicSize = (targetHeight / 200f) * scaleHeight;
         }
     }
 }
